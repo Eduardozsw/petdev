81a2fc5fe4fbf8ca4d3f334cd08ce2a090bfead2
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-02-12 22:05:42 -0300

    text

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,14 @@
+export function Hero() {
+  return (
+    <section className="bg-[#E84C3D] text-white relative overflow-hidden">
+      <div>
+        <article>
+          <div>
+            <h1>Seu pet merece cuidado, carinho e atenção especial</h1>
+            <p>Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amigo de quatro patas.</p>
+          </div>
+        </article>
+      </div>
+    </section>
+  )
+}
