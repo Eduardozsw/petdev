4945eae0b2a0ffdb358d8835c7989bd03f560e6d
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-02-14 23:43:38 -0300

    code review

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -6,15 +6,17 @@ export function About() {
   return (
     <section className="bg-[#FDF6ec] py-16">
       <div className="container mx-auto grid-cols-1 lg:grid-cols-2 items-center">
-        <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
-          <Image
-            src={catAndDog}
-            alt="Um cachorro e um gato."
-            fill
-            quality={100}
-            className="object-cover hover:scale-110 duration-300"
-            priority
-          />
+        <div>
+          <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
+            <Image
+              src={catAndDog}
+              alt="Um cachorro e um gato."
+              fill
+              quality={100}
+              className="object-cover hover:scale-110 duration-300"
+              priority
+            />
+          </div>
         </div>
       </div>
     </section>
