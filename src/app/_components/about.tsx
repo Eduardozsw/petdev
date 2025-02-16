0e1435649dea57cec8e0afd0be3df4db592249aa
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-02-16 14:38:38 -0300

    about section animated

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -9,7 +9,7 @@ export function About() {
     <section className="bg-[#FDF6ec] py-16">
       <div className="container px-4 mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-          <div className="relative">
+          <div className="relative" data-aos="fade-right">
             <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
               <Image
                 src={catAndDog}
@@ -31,7 +31,9 @@ export function About() {
               />
             </div>
           </div>
-          <div className="space-y-6 mt-10">
+          <div
+            data-aos="fade-left"
+            className="space-y-6 mt-10">
             <h2 className="text-4xl font-bold">SOBRE</h2>
             <p>
               Until one has loved an animal, a part of one's soul remains unawakened. We believe in it and we believe in easy access to things that are good for our mind, body and spirit. With a clever offering, superb support and a secure checkout you're in good hands.
@@ -54,7 +56,8 @@ export function About() {
 
             <div className="flex gap-2">
               <a
-                href="#"
+                target="_blank"
+                href={`https://wa.me/?text=Olá vim pelo site e gostaria de mais informações`} //entre a / e ? adicione o número para contato
                 className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
               >
                 <WhatsappLogo className="w-5 h-5 text-white" />
